f0c639f0c92283b53696af4d463d704dcddf89dd
mufid048 <20it048@charusat.edu.in>
2022-09-12 13:09:47 +0530

    Cors filer is added to handle axios request

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.demo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,31 +14,59 @@ import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Autowired
     DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication().dataSource(dataSource);
-
 //        auth.jdbcAuthentication().dataSource(dataSource)
 //                .withDefaultSchema()
-//                .withUser("admin")
-//                .password("pass")
-//                .roles("ADMIN", "USER")
-//                .and()
-//                .withUser("user")
-//                .password("pass")
-//                .roles("USER");
-
+//                .withUser(
+//                        User.withUsername("admin")
+//                                .password("admin")
+//                                .roles("ADMIN")
+//                )
+//                .withUser(
+//                        User.withUsername("user")
+//                                .password("user")
+//                                .roles("USER")
+//                );
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("admin")
+                .roles("ADMIN")
+                .and()
+                .withUser("user")
+                .password("user")
+                .roles("USER");
     }
-    @Override
+
+    //    @Override
     protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/login").permitAll()
+//                .anyRequest().authenticated().and()
+//                .formLogin();
+//        http.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+        http.authorizeRequests()
+                .antMatchers("/login").permitAll()
+                .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+        ;
         http.csrf().disable();
         http.headers().frameOptions().disable();
     }
+
     @Bean
-    public PasswordEncoder getPasswordEncoder(){
+    public PasswordEncoder getPasswordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
 
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
 }
--- src/main/java/com/example/demo/controller/MyController.java
+++ src/main/java/com/example/demo/controller/MyController.java
@@ -1,10 +1,18 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.Student;
+import com.example.demo.payload.LoginRequest;
 import com.example.demo.repository.studentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -12,37 +20,60 @@ import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins="http://localhost:3000")
-public class MyController {
+public class MyController<LoginRequest> {
     studentRepository studentrepository;
+    @Autowired
+    AuthenticationManager authenticationManager;
     @GetMapping
-    public String displayWelcomeMessage(){
-        return "<center><h1>Welcome to the Spring Boot Security!!!!</h1></center>";
+    public String getWelcomeMessage(){
+        return "<h1> Welcome to the world of Spring Boot!!!</h1>";
     }
     @Autowired
     @GetMapping("/listStudents")
     public List<Student> getAllStudents() {
         return studentrepository.findAll();
     }
-    @GetMapping("/student/{id}")
-    public Student getStudent(@PathVariable Integer id) {
-        return studentrepository.findById(id).get();
-    }
-    @DeleteMapping("/student/{id}") // delete from student where id=?
-    public List<Student> deleteStudent(@PathVariable Integer id) {
-        studentrepository.delete(studentrepository.findById(id).get());
-        return studentrepository.findAll();
-    }
-    @PostMapping("/student") // insert into student values(?, ?, ?)
-    public List<Student> addStudent(@RequestBody Student student) {
-        studentrepository.save(student);
-        return studentrepository.findAll();
-    }
-    @PutMapping("/student/{id}") // update table student set name=? where id=?
-    public List<Student> updateStudent(@RequestBody Student student, @PathVariable Integer id) {
-        Student studentObj = studentrepository.findById(id).get();
-        studentObj.setName(student.getName());
-        studentObj.setAddress(student.getAddress());
-        studentrepository.save(studentObj);
-        return studentrepository.findAll();
+//    @GetMapping("/student/{id}")
+//    public Student getStudent(@PathVariable Integer id) {
+//        return studentrepository.findById(id).get();
+//    }
+//    @DeleteMapping("/student/{id}") // delete from student where id=?
+//    public List<Student> deleteStudent(@PathVariable Integer id) {
+//        studentrepository.delete(studentrepository.findById(id).get());
+//        return studentrepository.findAll();
+//    }
+//    @PostMapping("/student") // insert into student values(?, ?, ?)
+//    public List<Student> addStudent(@RequestBody Student student) {
+//        studentrepository.save(student);
+//        return studentrepository.findAll();
+//    }
+//    @PutMapping("/student/{id}") // update table student set name=? where id=?
+//    public List<Student> updateStudent(@RequestBody Student student, @PathVariable Integer id) {
+//        Student studentObj = studentrepository.findById(id).get();
+//        studentObj.setName(student.getName());
+//        studentObj.setAddress(student.getAddress());
+//        studentrepository.save(studentObj);
+//        return studentrepository.findAll();
+//    }
+@PostMapping("/login")
+public String doLogin(@RequestBody com.example.demo.payload.LoginRequest loginRequest){
+    Authentication authentication = authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                    loginRequest.getUsername(),loginRequest.getPassword()
+            )
+    );
+
+    SecurityContextHolder.getContext().setAuthentication(authentication);
+    return "Login Successful";
+}
+
+    @RequestMapping(value="/logout", method=RequestMethod.GET)
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+        System.out.println("Logout");
+        org.springframework.security.core.Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "Logged out successful";
     }
 }
