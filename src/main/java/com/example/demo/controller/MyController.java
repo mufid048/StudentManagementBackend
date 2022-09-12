b89447def3c351d2aa45da2fcc2bf58ddaa203d8
mufid048 <20it048@charusat.edu.in>
2022-09-12 12:32:22 +0530

    first commit

---
+++ src/main/java/com/example/demo/controller/MyController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@RestController
+public class MyController {
+
+//    List<Student> students = new ArrayList<>(
+//            Arrays.asList(
+//                    new Student(1, "Tom", "US"),
+//                    new Student(2, "Harry", "Canada"),
+//                    new Student(3, "Nick", "UK")
+//            )
+//    );
+
+
+//    @GetMapping("/listStudents")
+//    public List<Student> getAllStudents(){
+//        return students;
+//    }
+}
