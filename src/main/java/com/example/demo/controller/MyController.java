d836b32f0682b159753c75c6b939e86a35281008
mufid048 <20it048@charusat.edu.in>
2022-09-12 12:33:54 +0530

    Added cross origin

--- src/main/java/com/example/demo/controller/MyController.java
+++ src/main/java/com/example/demo/controller/MyController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.Student;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 @RestController
+@CrossOrigin(origins="https://app-dummy123.herokuapp.com")
 public class MyController {
 
 //    List<Student> students = new ArrayList<>(
